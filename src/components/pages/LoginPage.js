f5e74077da166c31ef9032c923c1336d6a466b06
YannLescot <yann.lescot@hotmail.com>
2023-03-17 13:53:44 +0100

    ✨ F01: LOGIN > Ajout du formulaire et de son fonctionnement

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,12 +1,37 @@
+import React, { useState } from "react";
+
 export default function Login() {
   //State
+  const [name, setName] = useState("");
 
   //Comportements
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`Bonjour ${name}`);
+    setName("");
+  };
+
+  const handleInputChange = (e) => {
+    setName(e.target.value);
+  };
 
   //Affichage
   return (
     <div>
       <h1>Bienvenue chez nous !</h1>
+      <br />
+      <h2>Connectez-vous</h2>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Entrez votre prénom..."
+          value={name}
+          onChange={handleInputChange}
+          required
+        />
+        <button>Accédez à votre espace</button>
+      </form>
     </div>
   );
 }
