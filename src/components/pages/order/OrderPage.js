6904e3409bb998c0ba70cf934bf362cf52e05885
YannLescot <yann.lescot@hotmail.com>
2023-03-17 19:00:44 +0100

    ✨ F02: : OrderPage >  Récupération de l'inputValue

--- src/components/pages/order/OrderPage.js
+++ src/components/pages/order/OrderPage.js
@@ -1,10 +1,11 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 export default function OrderPage() {
+  const { inputValue } = useParams();
+  console.log(inputValue);
   return (
     <div>
-      <h1>Coucou</h1>
+      <h1>Coucou {inputValue}</h1>
       <Link to="/">
         <button>Déconnexion</button>
       </Link>
