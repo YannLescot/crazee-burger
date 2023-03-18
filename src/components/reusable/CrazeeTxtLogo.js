c831efca21830fe9c9690521b8b88086f543c395
YannLescot <yann.lescot@hotmail.com>
2023-03-18 17:09:40 +0100

    ✨ F03: CRAZEETXTLOGO > Styled the reusable text logo

--- src/components/reusable/CrazeeTxtLogo.js
+++ src/components/reusable/CrazeeTxtLogo.js
@@ -1,8 +1,31 @@
 import React from "react";
 import styled from "styled-components";
+import logo from "../../assets/images/F03_logo-orange.png";
+import { theme } from "../../theme/index.js";
 
 export default function CrazeeTxtLogo() {
-  return <CrazeeTxtLogoStyled>CrazeeLogo</CrazeeTxtLogoStyled>;
+  return (
+    <CrazeeTxtLogoStyled>
+      <h1>CRAZEE</h1>
+      <img src={logo} alt="logo" />
+      <h1>BURGER</h1>
+    </CrazeeTxtLogoStyled>
+  );
 }
 
-const CrazeeTxtLogoStyled = styled.div``;
+const CrazeeTxtLogoStyled = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: ${theme.spacing.xs};
+
+  h1 {
+    font-family: "Amatic SC", cursive;
+    font-size: 120px;
+    font-weight: ${theme.weights.bold};
+    color: ${theme.colors.primary_burger};
+  }
+  img {
+    width: 250px;
+  }
+`;
