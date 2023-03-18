d28c02dff3affa3ee748fae7b6f1359557ac7b8b
YannLescot <yann.lescot@hotmail.com>
2023-03-18 18:38:42 +0100

    🚧 F03: LOGINPAGE > Keeping track of not working background logic

--- src/components/pages/login/LoginPage.js
+++ src/components/pages/login/LoginPage.js
@@ -16,13 +16,13 @@ export default function Login() {
 
 const LoginStyled = styled.div`
   gap: ${theme.spacing.xl};
-  //background-color: aliceblue;
+  /*background-color: aliceblue;
   background-color: ${theme.colors.background_dark};
   background-image: linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)),
     url(${bgImage});
   background-size: cover;
   background-position: center;
-  opacity: 1;
+  opacity: 1;*/
 
   display: flex;
   flex-direction: column;
@@ -30,6 +30,21 @@ const LoginStyled = styled.div`
   align-items: center;
   height: 100vh;
   width: 100vw;
+
+  ::before {
+    content: "";
+    background: url(${bgImage});
+    background-size: cover;
+    background-position: center;
+    background-blend-mode: darken;
+
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: -1;
+  }
 
   .lineSeparator {
     flex-shrink: 0;
