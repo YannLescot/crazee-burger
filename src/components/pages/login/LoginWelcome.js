b14273dc1111c381ccc13d70c3594bbdb5c9c777
YannLescot <yann.lescot@hotmail.com>
2023-03-18 17:10:54 +0100

    ✨ F03: LOGINWELCOME > Style finalisation

--- src/components/pages/login/LoginWelcome.js
+++ src/components/pages/login/LoginWelcome.js
@@ -1,14 +1,25 @@
 import React from "react";
 import styled from "styled-components";
 import CrazeeTxtLogo from "../../reusable/CrazeeTxtLogo";
+import { theme } from "../../../theme/index.js";
 
 export default function LoginWelcome() {
   return (
     <LoginWelcomeStyled>
       <CrazeeTxtLogo />
-      <div>LoginWelcome</div>
+      <span>Bienvenue chez nous !</span>
     </LoginWelcomeStyled>
   );
 }
 
-const LoginWelcomeStyled = styled.div``;
+const LoginWelcomeStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  span {
+    font-family: "Amatic SC", cursive;
+    font-size: ${theme.fonts.P6};
+    font-weight: ${theme.weights.bold};
+    color: ${theme.colors.white};
+  }
+`;
