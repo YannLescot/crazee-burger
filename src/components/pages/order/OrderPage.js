32ebe3b0954438132155bac4deb3ee74a7cff68d
YannLescot <yann.lescot@hotmail.com>
2023-03-23 12:55:56 +0100

    💄  F04: OrderPage > Added background color

--- src/components/pages/order/OrderPage.js
+++ src/components/pages/order/OrderPage.js
@@ -1,15 +1,27 @@
 import { Link, useParams } from "react-router-dom";
+import styled from "styled-components";
+import { theme } from "../../../theme";
 
 export default function OrderPage() {
   //Le hook useParams permet de récupérer les paramètres de l'URL (passé dans le path de la route)
   const { username } = useParams();
 
   return (
-    <div>
+    <OrderPageStyled>
       <h1>Bonjour {username}</h1>
       <Link to="/">
         <button>Déconnexion</button>
       </Link>
-    </div>
+    </OrderPageStyled>
   );
 }
+
+const OrderPageStyled = styled.div`
+  display: flex;
+  justify-content: center;
+
+  height: 100vh;
+  width: 100vw;
+
+  background-color: ${theme.colors.primary};
+`;
