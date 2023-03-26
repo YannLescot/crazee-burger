66c1b27b0ae3083ffb912e258c4c722949c96ef2
YannLescot <yann.lescot@hotmail.com>
2023-03-26 14:10:05 +0200

    💄 ♻️   F05: ORDER > NavBar > Added delimitation border

--- src/components/pages/order/navBar/Navbar.js
+++ src/components/pages/order/navBar/Navbar.js
@@ -23,6 +23,7 @@ const NavbarStyled = styled.nav`
   background-color: ${theme.colors.white};
   border-top-left-radius: ${theme.borderRadius.extraRound};
   border-top-right-radius: ${theme.borderRadius.extraRound};
+  border-bottom: 1px solid ${theme.colors.greyLight};
   box-sizing: border-box;
   padding: 0 20px;
 
