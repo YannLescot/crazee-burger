9a74d71d28ce77938da0fff864da4b8dc6a8cd4c
YannLescot <yann.lescot@hotmail.com>
2023-03-28 22:48:46 +0200

    ✨  F06: ORDER > NAvBar > RightSide > Implemented toastNotifications

--- src/components/pages/order/navBar/NavbarRightSide.js
+++ src/components/pages/order/navBar/NavbarRightSide.js
@@ -3,13 +3,41 @@ import styled from "styled-components";
 import { theme } from "../../../../theme";
 import { useParams } from "react-router-dom";
 import UserProfile from "./UserProfile";
+import ToggleButton from "./ToggleButton";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function NavbarRightSide() {
   const { username } = useParams();
 
+  const [isChecked, setIsChecked] = React.useState(false);
+
+  function handleClick() {
+    setIsChecked(!isChecked);
+
+    !isChecked &&
+      toast.info("Mode admin activé", {
+        //icon: <FaUserSecret size={30} />,
+        theme: "dark",
+        position: "bottom-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+  }
+
   return (
     <NavbarRightSideStyled>
-      {<div className="adminbutton"> Admin Button</div>}
+      <ToastContainer className="toaster" bodyClassName="body-toast" />
+      <ToggleButton
+        isChecked={isChecked}
+        onToggle={handleClick}
+        labelIfChecked="Désactiver le mode admin"
+        labelIfUnchecked="Activer le mode admin"
+      />
       <UserProfile username={username} />
     </NavbarRightSideStyled>
   );
@@ -21,6 +49,25 @@ const NavbarRightSideStyled = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
+
+  .toaster {
+    max-width: 300px;
+  }
+
+  .Toastify__toast.Toastify__toast-theme--dark.Toastify__toast--info {
+    background: ${theme.colors.background_dark};
+  }
+
+  .body-toast {
+    .Toastify__toast-icon.Toastify--animate-icon.Toastify__zoom-enter {
+      margin-right: 20px;
+      margin-left: 5px;
+    }
+    div {
+      line-height: 1.3em;
+    }
+  }
+
   .adminbutton {
     background-color: red;
   }
