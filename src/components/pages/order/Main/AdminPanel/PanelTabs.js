b60f323165cf467e81fcdc1ace8df8e0bcc59584
YannLescot <yann.lescot@hotmail.com>
2023-03-29 22:34:42 +0200

    ♻️ F07 : ORDER > AdminPanel > PanelTabs > Onclick functions, new chevron imported

--- src/components/pages/order/Main/AdminPanel/PanelTabs.js
+++ src/components/pages/order/Main/AdminPanel/PanelTabs.js
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import { AiOutlinePlus } from "react-icons/ai";
-import { FiChevronDown } from "react-icons/fi";
+import { FiChevronDown, FiChevronUp } from "react-icons/fi";
 import { MdModeEditOutline } from "react-icons/md";
 import styled from "styled-components";
 import AdminContext from "../../../../../context/AdminContext";
@@ -10,13 +10,22 @@ export default function PanelTabs() {
   const { activeTab, setActiveTab, isPanelCollapsed, setIsPanelCollapsed } =
     useContext(AdminContext);
 
+  const handleCollapse = () => {
+    setIsPanelCollapsed(!isPanelCollapsed);
+  };
+
+  const handleClick = (e) => {
+    setActiveTab(e.target.id);
+    setIsPanelCollapsed(false);
+  };
+
   return (
     <PanelTabsStyled>
       <Tab
         id="collapse"
-        icon={<FiChevronDown />}
+        icon={isPanelCollapsed ? <FiChevronUp /> : <FiChevronDown />}
         label=""
-        onClick={() => {}}
+        onClick={handleCollapse}
         isActive={isPanelCollapsed}
       />
 
@@ -24,7 +33,7 @@ export default function PanelTabs() {
         id="add"
         icon={<AiOutlinePlus />}
         label="Ajouter un produit"
-        onClick={() => {}}
+        onClick={handleClick}
         isActive={activeTab === "add" ? true : false}
       />
 
@@ -32,7 +41,7 @@ export default function PanelTabs() {
         id="edit"
         icon={<MdModeEditOutline />}
         label="Modifier un produit"
-        onClick={() => {}}
+        onClick={handleClick}
         isActive={activeTab === "edit" ? true : false}
       />
     </PanelTabsStyled>
@@ -41,6 +50,7 @@ export default function PanelTabs() {
 
 const PanelTabsStyled = styled.div`
   margin-left: 71px;
+  height: 43px;
   display: flex;
   align-items: center;
 `;
