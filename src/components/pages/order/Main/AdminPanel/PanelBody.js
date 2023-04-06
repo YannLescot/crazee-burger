4835f5cf6b40af384b9b215cde6d1afde9d314fb
YannLescot <yann.lescot@hotmail.com>
2023-04-06 22:03:18 +0200

    💄 F08: ORDER > AdminPanel > PanelBody > AddProduct Layout

--- src/components/pages/order/Main/AdminPanel/PanelBody.js
+++ src/components/pages/order/Main/AdminPanel/PanelBody.js
@@ -13,7 +13,16 @@ export default function PanelBody() {
 
   return (
     <PanelBodyStyled>
-      {tabSelected && <p>{tabSelected.label}</p>}
+      {/*tabSelected && <p>{tabSelected.label}</p>*/}
+      <div className="addProductLayout">
+        <div className="imageProduct">IMG</div>{" "}
+        <div className="form">
+          <div className="textInput"></div>
+          <div className="textInput"></div>
+          <div className="textInput"></div>
+          <div className="addButton"></div>
+        </div>
+      </div>
     </PanelBodyStyled>
   );
 }
@@ -25,6 +34,37 @@ const PanelBodyStyled = styled.div`
   border-bottom-right-radius: ${theme.borderRadius.extraRound};
   border: 1px solid ${theme.colors.greyLight};
   box-shadow: ${theme.shadows.subtle};
+  padding: 40px 60px;
+  box-sizing: border-box;
+
+  .addProductLayout {
+    width: 50%
+    background-color: green;
+    display: grid;
+    grid-template-columns:  1fr 3fr 2fr;
+    grid-column-gap: 20px;
+    .imageProduct {
+      background-color: red;
+      width: 215px;
+      height: 120px;
+    }
+    .form {
+      background-color: blue;
+      display: grid;
+      grid-template-rows: repeat(4, 1fr);
+      grid-row-gap: 8px;
+      .textInput {
+        background-color: yellow;
+        width: 645px;
+        height: 35px;
+      }
+      .addButton {
+        background-color: orange;
+        width: 275px;
+        height: 35px;
+      }
+    }
+  }
 
   p {
     padding: 0 19px;
