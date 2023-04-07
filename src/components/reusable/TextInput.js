db9b5f1f9adbbfa91e792e3bfab96d1a7e0f2d2b
YannLescot <yann.lescot@hotmail.com>
2023-04-07 14:02:29 +0200

    ✨ 💄 F08: TextInput (R) > Added variant system

--- src/components/reusable/TextInput.js
+++ src/components/reusable/TextInput.js
@@ -1,21 +1,30 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { theme } from "../../theme/index.js";
 
-export default function TextInput({ value, onChange, Icon, ...extraProps }) {
+export default function TextInput({
+  value,
+  onChange,
+  placeholder,
+  Icon,
+  ...extraProps
+}) {
   return (
-    <TextInputStyled>
+    <TextInputStyled {...extraProps}>
       {Icon && Icon}
-      <input value={value} onChange={onChange} type="text" {...extraProps} />
+      <input
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+        type="text"
+      />
     </TextInputStyled>
   );
 }
 
 const TextInputStyled = styled.div`
-  gap: ${theme.spacing.xs};
   display: flex;
   align-items: center;
-  background-color: ${theme.colors.white};
   width: 100%;
   border: none;
   border-radius: ${theme.borderRadius.round};
@@ -29,15 +38,57 @@ const TextInputStyled = styled.div`
 
     &::placeholder {
       font-size: ${theme.fonts.size.SM};
-      background: ${theme.colors.white};
-      color: ${theme.colors.greyLight};
+    }
+
+    .icon {
+      font-size: ${theme.fonts.size.SM};
     }
   }
 
-  .icon {
-    color: ${theme.colors.greyBlue};
-    margin-left: ${theme.spacing.md};
-    font-size: ${theme.fonts.size.SM};
-    padding: 0px 10px;
-  }
+  ${(extraProps) =>
+    extraProps.variant === "minimalist"
+      ? getMinimalistStyle()
+      : getNormalStyle()}
 `;
+
+const getNormalStyle = () => {
+  return css`
+    background-color: ${theme.colors.white};
+    gap: ${theme.spacing.xs};
+
+    input {
+      width: 70%;
+
+      &::placeholder {
+        background: ${theme.colors.white};
+        color: ${theme.colors.greyLight};
+      }
+    }
+
+    .icon {
+      color: ${theme.colors.greyBlue};
+      margin-left: ${theme.spacing.md};
+      padding: 0px 10px;
+    }
+  `;
+};
+
+const getMinimalistStyle = () => {
+  return css`
+    background-color: ${theme.colors.background_white};
+
+    input {
+      background-color: ${theme.colors.background_white};
+      margin-left: 15px;
+
+      &::placeholder {
+        color: ${theme.colors.greyMedium};
+      }
+    }
+
+    .icon {
+      color: ${theme.colors.greyBlue};
+      margin-left: 25px;
+    }
+  `;
+};
