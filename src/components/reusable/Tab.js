113847f05d7d780bfe2d2e9c3337792c78e6bb11
YannLescot <yann.lescot@hotmail.com>
2023-04-07 23:04:24 +0200

    ♻️  F08: Tab (R) > font color corrected

--- src/components/reusable/Tab.js
+++ src/components/reusable/Tab.js
@@ -22,7 +22,7 @@ const TabStyled = styled.button`
   border-color: ${theme.colors.greyLight};
   border-width: 1px 1px 2px 1px;
   font-size: ${theme.fonts.size.P0};
-  color: ${theme.colors.greyBlue};
+  color: ${theme.colors.greySemiDark};
   border-top-left-radius: ${theme.borderRadius.round};
   border-top-right-radius: ${theme.borderRadius.round};
   box-shadow: ${theme.shadows.subtle};
