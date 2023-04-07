5b6d799762d670e3db57a14c19757a4196ac86db
YannLescot <yann.lescot@hotmail.com>
2023-04-07 19:34:19 +0200

    ✨ F08: ORDER > AdminPanel > tabsConfig> Add content

--- src/components/pages/order/Main/AdminPanel/tabsConfig.js
+++ src/components/pages/order/Main/AdminPanel/tabsConfig.js
@@ -1,16 +1,19 @@
 import { AiOutlinePlus } from "react-icons/ai";
 import { MdModeEditOutline } from "react-icons/md";
+import AddProduct from "./AddProduct/AddProduct";
 
 export const tabsConfig = [
   {
     id: "add",
     Icon: <AiOutlinePlus />,
     label: "Ajouter un produit",
+    content: <AddProduct />,
   },
   {
     id: "edit",
     Icon: <MdModeEditOutline />,
     label: "Modifier un produit",
+    content: "Modifier un produit",
   },
 ];
 
