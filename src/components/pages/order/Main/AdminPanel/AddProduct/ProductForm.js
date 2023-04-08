6e20c86327e3bf3582b7a9ca88c083a9f4fb6dfd
YannLescot <yann.lescot@hotmail.com>
2023-04-08 17:19:59 +0200

    ♻️ F09: ORDER > AdminPanel > AddForm renamed to ProductForm

---
+++ src/components/pages/order/Main/AdminPanel/AddProduct/ProductForm.js
@@ -0,0 +1,95 @@
+import React, { useContext } from "react";
+import { FiCheck } from "react-icons/fi";
+import styled from "styled-components";
+import { theme } from "../../../../../../theme";
+import Button from "../../../../../reusable/Button";
+import TextInput from "../../../../../reusable/TextInput";
+import { InputsConfig } from "./inputsConfig";
+import OrderContext from "../../../../../../context/OrderContext";
+
+export default function ProductForm({ onSubmit, wasProductAdded, tab }) {
+  const {
+    productToAdd,
+    setProductToAdd,
+    productToEdit,
+    setProductToEdit,
+    menu,
+    setMenu,
+  } = useContext(OrderContext);
+
+  const handleFieldChange = (fieldName, value, tab) => {
+    if (tab === "add") {
+      setProductToAdd({ ...productToAdd, [fieldName]: value });
+    } else if (tab === "edit") {
+      setProductToEdit({ ...productToEdit, [fieldName]: value });
+
+      const newMenu = menu.map((product) =>
+        product.id === productToEdit.id
+          ? { ...product, [fieldName]: value }
+          : product
+      );
+      setMenu(newMenu);
+    }
+  };
+
+  return (
+    <ProductFormStyled onSubmit={onSubmit}>
+      {InputsConfig(tab).map(({ type, value, field, placeholder, Icon }) => {
+        return (
+          <TextInput
+            key={field}
+            type={type && type}
+            value={value}
+            onChange={(e) => handleFieldChange(field, e.target.value, tab)}
+            placeholder={placeholder}
+            Icon={Icon}
+            variant="minimalist"
+          />
+        );
+      })}
+
+      {tab === "add" && (
+        <div className="addSection">
+          <Button label={"Ajouter un nouveau produit au menu"} variant="add" />
+          {wasProductAdded && (
+            <span className="succesAdd">
+              <FiCheck className="icon" /> Ajouté avec succès !
+            </span>
+          )}
+        </div>
+      )}
+
+      {tab === "edit" && (
+        <div className="editSection">
+          <p>Modifie un produit en temps réel</p>
+        </div>
+      )}
+    </ProductFormStyled>
+  );
+}
+
+const ProductFormStyled = styled.form`
+  display: grid;
+  grid-template-rows: repeat(4, 1fr);
+  grid-row-gap: 8px;
+  height: 165px;
+  div {
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    height: 34px;
+    span {
+      display: flex;
+      align-items: center;
+      font-size: ${theme.fonts.size.SM};
+      margin: 8px 18px;
+      color: ${theme.colors.success};
+
+      .icon {
+        border: 1px solid ${theme.colors.success};
+        border-radius: ${theme.borderRadius.circle};
+        margin-right: 8px;
+      }
+    }
+  }
+`;
