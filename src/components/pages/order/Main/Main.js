a020c3364aa5e9cb88847d05bbded3c0adb50885
YannLescot <yann.lescot@hotmail.com>
2023-04-18 23:08:18 +0200

    💄 ✨ F10: MAIN > Basket > Layout

--- src/components/pages/order/Main/Main.js
+++ src/components/pages/order/Main/Main.js
@@ -10,7 +10,14 @@ export default function Main() {
 
   return (
     <MainStyled>
-      {/*<div className="basket">Basket</div>*/}
+      <div className="basket">
+        <div className="header">
+          <div className="total">TOTAL</div>
+          <div className="price">PRICE</div>
+        </div>
+        <div className="body">BODY</div>
+        <div className="footer">FOOTER</div>
+      </div>
       <div className="menu-and-admin">
         <Menu />
         {isAdmin && <AdminPanel />}
@@ -22,7 +29,7 @@ export default function Main() {
 const MainStyled = styled.div`
   flex: 1;
   display: grid;
-  //grid-template-columns: 25% 1fr;
+  grid-template-columns: 25% 1fr;
   height: calc(95vh - 10vh);
   background-color: ${theme.colors.background_white};
   border-bottom-left-radius: ${theme.borderRadius.extraRound};
@@ -35,7 +42,40 @@ const MainStyled = styled.div`
     overflow-y: hidden;
   }
 
-  /*.basket {
+  .basket {
+    display: grid;
+    grid-template-rows: 9% 1fr 9%;
     background: pink;
-  }*/
+
+    .header {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding: 12px 18px;
+      background: red;
+
+      .total {
+        background: yellow;
+      }
+
+      .price {
+        background: orange;
+      }
+    }
+
+    .footer {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      background: green;
+    }
+
+    .body {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      background: blue;
+    }
+  }
 `;
