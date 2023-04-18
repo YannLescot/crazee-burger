1e1cac5afc5a051e2261c4e914489949492fe464
YannLescot <yann.lescot@hotmail.com>
2023-04-18 23:21:01 +0200

    ✨ 💄 F10: MAIN > Basket > Header > Styled

--- src/components/pages/order/Main/Basket/BasketHeader.js
+++ src/components/pages/order/Main/Basket/BasketHeader.js
@@ -1,11 +1,12 @@
 import React from "react";
 import styled from "styled-components";
+import { theme } from "../../../../../theme";
 
 export default function BasketHeader() {
   return (
     <BasketHeaderStyled>
-      <div className="total">TOTAL</div>
-      <div className="price">PRICE</div>
+      <p className="total">TOTAL</p>
+      <p className="price">0,00 €</p>
     </BasketHeaderStyled>
   );
 }
@@ -15,13 +16,19 @@ const BasketHeaderStyled = styled.div`
   justify-content: space-between;
   align-items: center;
   padding: 12px 18px;
-  background: red;
+  background: ${theme.colors.background_dark};
+
+  p {
+    font-family: ${theme.font.families.stylish};
+    font-size: ${theme.font.sizes.P4};
+    color: ${theme.colors.primary};
+  }
 
   .total {
-    background: yellow;
+    font-weight: ${theme.font.weights.regular};
   }
 
   .price {
-    background: orange;
+    font-weight: ${theme.font.weights.bold};
   }
 `;
