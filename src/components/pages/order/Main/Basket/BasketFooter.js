933ea98d09d78496885a10166cacbafe86c38485
YannLescot <yann.lescot@hotmail.com>
2023-04-18 23:24:35 +0200

    ✨ 💄 F10: MAIN > Basket > Footer > Styled

--- src/components/pages/order/Main/Basket/BasketFooter.js
+++ src/components/pages/order/Main/Basket/BasketFooter.js
@@ -1,13 +1,19 @@
 import React from "react";
 import styled from "styled-components";
+import { theme } from "../../../../../theme";
 
 export default function BasketFooter() {
-  return <BasketFooterStyled>FOOTER</BasketFooterStyled>;
+  return <BasketFooterStyled>Codé avec 🥰 et React.JS</BasketFooterStyled>;
 }
 
 const BasketFooterStyled = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  background: green;
+  background: ${theme.colors.background_dark};
+  border-bottom-left-radius: ${theme.borderRadius.extraRound};
+
+  font-size: ${theme.font.sizes.P2};
+  font-family: ${theme.font.families.stylish};
+  color: ${theme.colors.white};
 `;
