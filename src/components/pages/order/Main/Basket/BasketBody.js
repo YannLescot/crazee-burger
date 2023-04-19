26a82eb4c55122da96b77c1825b3865d230aaa64
YannLescot <yann.lescot@hotmail.com>
2023-04-19 18:21:18 +0200

    ♻️ ✨   F11: ORDER > Basket > Create BasketCard(R) and added jsx and css

--- src/components/pages/order/Main/Basket/BasketBody.js
+++ src/components/pages/order/Main/Basket/BasketBody.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useContext } from "react";
 import styled from "styled-components";
 import { theme } from "../../../../../theme";
+import BasketCard from "../../../../reusable/BasketCard";
+import OrderContext from "../../../../../context/OrderContext";
 
 export default function BasketBody() {
+  const { basket, menu } = useContext(OrderContext);
+
   return (
     <BasketBodyStyled>
-      <span>Votre commande est vide.</span>
+      {basket.length === 0 ? (
+        <div className="empty">Votre commande est vide.</div>
+      ) : (
+        basket.map((product) => {
+          const productInfo = menu.find((item) => item.id === product.id);
+          return (
+            <BasketCard
+              key={product.id}
+              imageSource={productInfo.image}
+              title={productInfo.title}
+              price={productInfo.price}
+              quantity={product.quantity}
+            />
+          );
+        })
+      )}
     </BasketBodyStyled>
   );
 }
@@ -13,6 +32,7 @@ export default function BasketBody() {
 const BasketBodyStyled = styled.div`
   display: flex;
   flex-direction: column;
+  justify-content: center;
   align-items: center;
   background: ${theme.colors.background_white};
 
@@ -20,60 +40,20 @@ const BasketBodyStyled = styled.div`
   overflow-y: scroll;
 
   z-index: 2;
+
   .empty {
-    justify-content: center;
+    text-align: center;
     font-size: ${theme.font.sizes.P4};
     font-family: ${theme.font.families.stylish};
     color: ${theme.colors.greyBlue};
   }
 
   .produits {
+    align-items: flex-start;
     width: 100%;
     display: flex;
     flex-direction: column;
     align-items: center;
     margin-top: 20px;
-
-    .product {
-      display: grid;
-      grid-template-columns: 1fr 3fr 1fr 1fr;
-      align-items: center;
-      box-sizing: border-box;
-      width: 90%;
-      height: 85px;
-      padding: 8px 16px;
-      margin-bottom: 20px;
-      border-radius: ${theme.borderRadius.extraRound};
-
-      background-color: red;
-
-      .image {
-        width: 85px;
-        height: 60px;
-        background-color: aliceblue;
-
-        object-fit: contain;
-      }
-
-      .description {
-        margin-left: 14px;
-
-        .title {
-          font-size: ${theme.font.sizes.P3};
-          line-height: 32px;
-          font-family: ${theme.font.families.stylish};
-          font-weight: ${theme.font.weights.bold};
-        }
-
-        .prix {
-          color: ${theme.colors.primary};
-          font-size: ${theme.font.sizes.SM};
-        }
-      }
-
-      .quantity {
-        color: ${theme.colors.primary};
-      }
-    }
   }
 `;
---
+++ src/components/reusable/BasketCard.js
@@ -0,0 +1,59 @@
+import React from "react";
+import styled from "styled-components";
+import { theme } from "../../theme";
+
+export default function BasketCard({ imageSource, title, price, quantity }) {
+  return (
+    <BasketCardStyled>
+      <img src={imageSource} alt="produit" />
+      <div className="description">
+        <div className="title">{title}</div>
+        <div className="prix">{price}</div>
+      </div>
+      <div className="quantity">x{quantity}</div>
+    </BasketCardStyled>
+  );
+}
+
+const BasketCardStyled = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 3fr 1fr 1fr;
+  align-items: center;
+  box-sizing: border-box;
+  width: 90%;
+  height: 85px;
+  padding: 8px 16px;
+  margin-bottom: 20px;
+  border-radius: ${theme.borderRadius.extraRound};
+
+  background-color: ${theme.colors.background_white};
+  box-shadow: ${theme.shadows.medium};
+
+  img {
+    width: 85px;
+    height: 60px;
+    background-color: aliceblue;
+
+    object-fit: contain;
+  }
+
+  .description {
+    margin-left: 14px;
+
+    .title {
+      font-size: ${theme.font.sizes.P3};
+      line-height: 32px;
+      font-family: ${theme.font.families.stylish};
+      font-weight: ${theme.font.weights.bold};
+    }
+
+    .prix {
+      color: ${theme.colors.primary};
+      font-size: ${theme.font.sizes.SM};
+    }
+  }
+
+  .quantity {
+    color: ${theme.colors.primary};
+  }
+`;
