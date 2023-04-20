b236995c63b353ad1af8518871f8b1a5773a7b6e
YannLescot <yann.lescot@hotmail.com>
2023-04-20 15:52:31 +0200

    ♻️  F12: EmptyMenu > God rid of code duplication CSS

--- src/components/pages/order/Main/Menu/EmptyMenu.js
+++ src/components/pages/order/Main/Menu/EmptyMenu.js
@@ -24,29 +24,26 @@ const EmptyMenuStyled = styled.div`
 
   box-shadow: ${theme.shadows.strong};
 
+  font-family: ${theme.font.families.stylish};
+  color: ${theme.colors.greyBlue};
+
   h1 {
-    font-family: ${theme.font.families.stylish};
     font-size: ${theme.font.sizes.P4};
     font-weight: ${theme.font.weights.bold};
-    color: ${theme.colors.greyBlue};
-    margin-bottom: 31px;
     margin-top: 0px;
+    margin-bottom: 31px;
   }
 
   h2 {
-    font-family: ${theme.font.families.stylish};
     font-size: ${theme.font.sizes.P4};
     font-weight: ${theme.font.weights.regular};
-    color: ${theme.colors.greyBlue};
     margin-top: 0px;
     margin-bottom: 31px;
   }
 
   h3 {
-    font-family: ${theme.font.families.stylish};
     font-size: ${theme.font.sizes.P4};
     font-weight: ${theme.font.weights.regular};
-    color: ${theme.colors.greyBlue};
     margin-top: 0px;
     margin-bottom: 0px;
   }
