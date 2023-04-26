f26e7e09cba604386f5278d085ea447b29166c52
YannLescot <yann.lescot@hotmail.com>
2023-04-26 17:35:06 +0200

    🚧  F13: window.js > Added localstorage functions

--- src/utils/window.js
+++ src/utils/window.js
@@ -1,3 +1,12 @@
 export const refreshPage = () => {
   window.location.reload();
 };
+
+export const storeLocally = (key, value) => {
+  window.localStorage.setItem(key, JSON.stringify(value));
+};
+
+export const retrieveFromLocalStorage = (key) => {
+  if (!window.localStorage.getItem(key)) return null;
+  return JSON.parse(window.localStorage.getItem(key));
+};
