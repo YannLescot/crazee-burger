83dae0b46b0e6035faa69be7005adf11a9082657
YannLescot <yann.lescot@hotmail.com>
2023-06-18 15:37:31 +0200

    🚧  R03 : Typescript translation progress (custom hooks)

--- src/hooks/useDatabase.tsx
+++ src/hooks/useDatabase.tsx
@@ -1,12 +1,13 @@
 import { ref, set, get } from "firebase/database";
 import database from "../data/firebaseConfig";
+import { Product } from "../utils/interfaces";
 
-function saveUserMenu(username, menu) {
+function saveUserMenu(username: string, menu: Product[]) {
   const userRef = ref(database, "users/" + username);
   set(userRef, { menu: menu, loggedInOnce: true });
 }
 
-async function getUserMenu(username, callback) {
+async function getUserMenu(username: string, callback: Function) {
   const userRef = ref(database, "users/" + username);
   const snapshot = await get(userRef);
 
