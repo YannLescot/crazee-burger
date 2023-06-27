49e9e922534da5fe4f6e508391c8ee23c840a5d6
YannLescot <yann.lescot@hotmail.com>
2023-06-27 15:40:42 +0200

    🔐 F13B: Add all the firebase config to env

--- src/data/firebaseConfig.tsx
+++ src/data/firebaseConfig.tsx
@@ -2,15 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: "burgermvp-ae75d.firebaseapp.com",
-  databaseURL:
-    "https://burgermvp-ae75d-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "burgermvp-ae75d",
-  storageBucket: "burgermvp-ae75d.appspot.com",
-  messagingSenderId: "415818110353",
-  appId: "1:415818110353:web:08eb05b9619c1e89377630",
-  measurementId: "G-S3570MRXL7",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
